4075cb95dfc3b11aed78db438daf1701e3de633d
ImSreyasCollege <sreyassatheesh755@gmail.com>
2023-10-16 15:23:16 +0530

    q 23 code refactore

--- fourDigitSquareList.py
+++ fourDigitSquareList.py
@@ -1,11 +1,7 @@
 from math import sqrt as s
 a = int(input("enter the start : "))
 b = int(input("enter the end : "))
-start = -1
-for i in range(a, b+1):
-    if s(i) == int(s(i)):
-        start = s(i)
-        break
+
 list = [i for i in range(a, b+1) if s(i) == int(s(i)) and len(str(i)) == 4]
 final = []
 for num in list:

70ecb6caa74c2b0b05cf116a153f832b0d68886e
sandra <sandrasaji243@gmail.com>
2023-10-30 16:28:44 +0530

    q 32 completed

--- mod.py
+++ mod.py
@@ -1,16 +1,16 @@
-from graphics.circle import area as areaOfCircle, perimeter as perimeterOfCircle
+from graphics.circle import *
 from graphics.rectangle import area as areaOfRectangle, perimeter as perimeterOfRectangle
 from graphics._3Dgraphics.cuboid import area as areaOfCuboid, perimeter as perimeterOfCuboid
 from graphics._3Dgraphics.sphere import area as areaOfSphere, perimeter as perimeterOfSphere
 
 # circle 
 r  = int(input("enter the radius of circle : "))
-print("area of circle is : ", areaOfCircle(r))
-print("perimeter of circle is : ", perimeterOfCircle(r), "\n")
+print("area of circle is : ", area(r))
+print("perimeter of circle is : ", perimeter(r), "\n")
 
 # rectangle 
-l = int(input("enter the length : "))
-b = int(input("enter the breadth : "))
+l = int(input("enter the length of rectangle : "))
+b = int(input("enter the breadth of rectangle : "))
 print("area of rectangle is : ", areaOfRectangle(l, b))
 print("perimeter of rectangle is : ", perimeterOfRectangle(l, b), "\n")
 
--- replaceOcc.py
+++ replaceOcc.py
@@ -1,6 +1,2 @@
 a = input("enter a string : ")
-# b = [a[0]]
-# for i in range(1, len(a)):
-#     b.append("$" if a[i] == a[0] else a[i])
-# print("".join(b))
 print(a[0] + "".join(["$" if i == a[0] else i for i in a[1:]]))

dbd8c051130b609b58900de54514f2e81281876f
ImSreyasCollege <sreyassatheesh755@gmail.com>
2023-12-09 23:41:58 +0530

    program 39 code changed

--- prgm39.py
+++ prgm39.py
@@ -1,20 +1,21 @@
-a = open("demo.txt", "r")
-b = open("t", "w")
-c = a.readlines()
-d = len(c)
-for i in range(0, d):
-    if i % 2 == 0:
-        b.write(c[i])
-    else:
-        pass
-b.close()
-b = open("t", "r")
-e = b.read()
-print(e)
-a.close()
-b.close()
-
-
+# a = open("demo.txt", "r")
+# b = open("t", "w")
+# c = a.readlines()
+# for i in range(0, len(c), 2):
+#     b.write(c[i])
+# b.close()
+# b = open("t", "r")
+# e = b.read()
+# print(e)
+# a.close()
+# b.close()
+with open("demo.txt", "r") as readFile:
+    lines = readFile.readlines();
+    with open("new.txt", "w") as writeFile:
+        for i in range(0, len(lines), 2):
+            writeFile.write(lines[i])
+with open("new.txt", "r") as displayFile :
+    print(displayFile.read())
 
 
 
